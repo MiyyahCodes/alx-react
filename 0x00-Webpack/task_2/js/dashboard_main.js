726cba03a431a13a120c2730a6424257a0d13ff5
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2023-01-27 09:53:44 -0800

    Update dashboard_main.js

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,20 +1,18 @@
-import '../css/main.css';
-const $ = require('jquery');
-const _ = require('lodash');
+
+import $ from 'jquery';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-const updateCounter = () => {
-	let times = $('#count').html() || 0;
-	$('button').on('click', () => {
-		times++;
-		$('#count').html(`${times} clicks on the button`);
-	});
+let conunt = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
 };
 
-_.debounce(updateCounter, 500);
-updateCounter();
+$('button').on('click', _.debounce(updateCounter, 500));
