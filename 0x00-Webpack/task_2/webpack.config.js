0b25163b6a1d1dcc01fa52bf549152af29261148
MiyyahCodes <adebimpeadefila64@gmail.com>
2023-01-27 18:42:47 -0800

    test

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		main: path.resolve(__dirname, './js/dashboard_main.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js',
+	},
+	performance: {
+		maxAssetSize: 1000000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(gif|svg|png|jpg|jpeg)$/i,
+				type: 'asset/resource',
+				use: [
+					{
+						loader: ['file-loader', 'image-webpack-loader'],
+						options: {
+							bypassOnDebug: true,
+							disable: true,
+						},
+					},
+				],
+			},
+		],
+	},
+};
